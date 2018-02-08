451f4673c99f900e9e20fc90212a65ccd3648e9d
a7medkamtcho <iamamazingplusboy@gmail.com>
2018-02-08 13:26:24 +0200

    Delete send_sms.py

--- send_sms.py
+++
@@ -1,5 +0,0 @@
-from twilio.rest import TwilioRestClient
-client = TwilioRestClient()
-client.messages.create(from_="(718) 673-2372",
-                       to="(347) 695-8315",
-                       body="hello its just test")
